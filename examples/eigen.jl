b77b6a0bb462861ed0cfec38ba7c944ae92a6f3e
Abigail Jones <abigailjones@pobox.com>
2025-09-30 14:46:41 -0500

    add constraint in eigen example

--- examples/eigen.jl
+++ examples/eigen.jl
@@ -15,9 +15,9 @@ occurring.
 
 include("../rigid_hom.jl")
 
-num_funcs = 2
-num_vars = num_funcs + 1
-M = rand(Float64,(num_funcs,num_funcs)) + im*rand(Float64,(num_funcs,num_funcs))
+mat_dims = 2
+num_vars = mat_dims + 1
+M = rand(Float64,(mat_dims,mat_dims)) + im*rand(Float64,(mat_dims,mat_dims))
 # make A a skew Hermitian matrix (then all eigenvalues are complex, and if A
 # is invertible, it will follow that log(A) is defined)
 # (since M is random, it is probably true that A is invertible)
@@ -26,7 +26,13 @@ A = M - M'
 # we are computing eivalue/eigenvector pairs, where X = [v, lambda]
 # the start_system (and resulting path) are chosen so that lambda and v do not
 # get mixed together as we move along the path
-F = [X -> sum(A[idx,:] .* X[1:end-1]) - X[end]*X[idx] for idx in 1:num_funcs]
+F = []
+for idx in 1:mat_dims
+    push!(F, X -> sum(A[idx,:] .* X[1:end-1]) - X[end]*X[idx])
+end
+# add a final constraint that forces eigenvectors away from the zero vector
+push!(F, X -> sum(abs2.(X[1:end-1])) - 1)
+num_funcs = length(F)
 
 max_degree = 2
 max_iter = 10000
@@ -39,9 +45,9 @@ start_root = push!(V, eivals[1])
 
 target_system = []
 for idx in 1:num_funcs
-    mat = rand(Float64,(num_funcs+1,num_funcs+1)) + im*rand(Float64,(num_funcs+1,num_funcs+1))
-    mat[:,end] = zeros(num_funcs+1)
-    mat[end,:] = zeros(num_funcs+1)
+    mat = rand(Float64,(num_vars,num_vars)) + im*rand(Float64,(num_vars,num_vars))
+    mat[:,end] = zeros(num_vars)
+    mat[end,:] = zeros(num_vars)
     mat[end, end] = 1
     push!(target_system, mat)
 end
@@ -52,3 +58,6 @@ path = t -> [exp((1-t) * log(mat)) for mat in target_system]
 
 final_root, _ = solve(F, num_funcs, num_vars, max_degree, max_iter, use_heuristic,
                       start_system, start_root, path)
+# this last line is just a janky way of forcing julia to not print the return
+# statement of the previous line, which just clutters up the terminal window
+println()
